7392e575420c5cf04a11c7a2010416b9bb9f9a41
Pavel Starovoitov <pollstar27@gmail.com>
2022-07-24 14:36:05 +0300

    Edit pom.xml

---
+++ src/test/java/ua/pollstar/spring_security/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.pollstar.spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
